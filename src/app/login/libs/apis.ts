c63f8be1d5d81b0b6f1c5564e5f3e2824bb97bf8
cm-lee <thebrightlee1@gmail.com>
2023-10-07 15:31:29 +0900

    로그인 api 작업중

---
+++ src/app/login/libs/apis.ts
@@ -0,0 +1,21 @@
+interface ILoginInfo {
+    userId: string;
+    password: string;
+}
+
+export async function login(loginInfo: ILoginInfo) {
+    const res = await fetch("/member/login", {
+        method: "POST",
+        body: JSON.stringify(loginInfo),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+}

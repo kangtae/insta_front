b50b145e0b4486721e14b2b675dfdf14c845369b
bright <lcm@brightui-MacBookPro.local>
2023-11-05 20:35:08 +0900

    라우트 그룹 적용(샘플)

---
+++ src/app/(sample)/test/page.tsx
@@ -0,0 +1,8 @@
+import Gnb from "@/components/Gnb";
+import Sidebar from "@/components/Sidebar";
+
+export default function SamplePage(){
+    return <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eveniet mollitia perferendis placeat dolorum, magnam inventore aut consectetur asperiores similique earum aliquam beatae vel nulla error eius enim? Repellat, necessitatibus?
+    </>
+}

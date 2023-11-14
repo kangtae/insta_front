395fd8df671e680069707c148c94792a57702395
fe-cmlee <cmlee@braincommerce.com>
2023-11-14 18:42:29 +0900

    로그인 react-hook-form 적용, yarn.lock git ignore에 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,49 +9,29 @@ import LoginItem from "@/components/LoginItem";
 
 import { login } from "./libs/apis";
 import { ILoginInfo } from "./libs/types";
+import { FieldValues, useForm } from "react-hook-form";
+import LoginInput from "@/components/LoginInput";
 
 export default function LoginPage() {
-    const [loginInfo, setLoginInfo] = useState<ILoginInfo>({
-        userId: "",
-        password: "",
-    });
+  const {register , handleSubmit, setError, formState: {errors}} = useForm()
 
-    const handleSubmit = async (event: React.SyntheticEvent) => {
-        event.preventDefault();
-        try {
-            console.log(loginInfo);
-
-            const result = await login(loginInfo);
-            console.log(result);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    const handleChange =
-        (key: string) => (event: ChangeEvent<HTMLInputElement>) => {
-            setLoginInfo((prev) => ({
-                ...prev,
-                [key]: event.target.value,
-            }));
-        };
+  const onClickSubmit = async (data: FieldValues)=>{
+    // console.log(data)
+    try {
+        const result = await login(data as ILoginInfo);
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+  }
 
     return (
         <>
             <h1>로그인 페이지</h1>
             <div className="w-[300px] mx-auto">
-                <form onSubmit={handleSubmit}>
-                    <LoginItem
-                        label="아이디&휴대폰 번호"
-                        inputType="text"
-                        placeholder="아이디를 입력하시기 바랍니다"
-                        handleChange={handleChange("userId")}
-                    />
-                    <LoginItem
-                        label="비밀번호"
-                        inputType="password"
-                        placeholder="비밀번호를 입력하시기 바랍니다"
-                        handleChange={handleChange("password")}
-                    />
+                <form onSubmit={handleSubmit(onClickSubmit)}>
+                    <LoginInput register={register} reg_msg="아이디를 입력하세요." label="아이디&휴대폰 번호" name="userId" inputType="text" placeholder="아이디를 입력하세요." errors={errors}/>
+                    <LoginInput register={register} reg_msg="비밀번호를 입력하세요." label="비밀번호" name="password" inputType="password" placeholder="비밀번호를 입력하세요." errors={errors}/>
                     <LoginButton
                         buttonType="submit"
                         buttonText="로그인"

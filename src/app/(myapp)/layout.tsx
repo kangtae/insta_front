5e4da19b561c22ad6484332479902ad811e34470
cm-lee <thebrightlee1@gmail.com>
2023-11-23 17:27:20 +0900

    마이페이지 ui 완료, 레이아웃 변경, sample 폴더 삭제

--- src/app/(myapp)/layout.tsx
+++ src/app/(myapp)/layout.tsx
@@ -1,16 +1,14 @@
 import Gnb from "@/components/Gnb"
-import Sidebar from "@/components/Sidebar"
 
-export default function SampleLayout({
+export default function MyAppLayout({
     children, // will be a page or nested layout
   }: {
     children: React.ReactNode
   }) {
     return (
-        <>
+      <>
         <Gnb/>
         <div className="w-full p-6">{children}</div>
-        <Sidebar />
-        </>
+      </>
     )
   }
--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,10 +1,16 @@
+"use client"
+
 import { HeartFilled } from "@ant-design/icons";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function CardItem(){
-  return <>
-    <div>
-      <div className="relative aspect-square">
+  const handleDelete = (id: number) => {
+
+  }
+
+  return <Link href="/">
+    <div className="relative aspect-square">
         <Image src="/temp_img_2.jpeg" alt="" fill={true} className="object-cover" />
       </div>
       <div className="mt-3">
@@ -12,7 +18,10 @@ export default function CardItem(){
           <HeartFilled />
           <p className="ml-1">친구1 외 120명이 좋아합니다</p>
         </div>
+        <div className="flex items-center justify-between mt-1">
+          <p>댓글 총 0개</p>
+          <button className="text-red-400" onClick={() => handleDelete(1)}>삭제</button>
+        </div>
       </div>
-    </div>
-  </>
+  </Link>
 }
